22aa809b8ea9c0cf8e62625e504882ede0df7632
Jacob Banta <106040174+JacobBanta@users.noreply.github.com>
2024-05-11 20:38:11 -0400

    start menu i guess

---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    public GameObject startButton;
+    public GameObject planetSelector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        //Debug.Log("start");
+        startButton.SetActive(false);
+        planetSelector.SetActive(true);
+    }
+    public void ChoosePlanet()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
